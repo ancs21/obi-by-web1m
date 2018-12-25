a952ad435c43804c07f9033da3756714776143bb
Truong An <ancs21.ps@gmail.com>
2018-12-26 02:07:16 +0700

    Update fb.js

--- ui/src/utils/fb.js
+++ ui/src/utils/fb.js
@@ -3,12 +3,7 @@ import '@firebase/storage'
 import '@firebase/auth'
 
 const config = {
-  apiKey: 'AIzaSyA-D0Ey8eWujtqTEmBPbvE1IoXodvQtrRg',
-  authDomain: 'tracking-59c9e.firebaseapp.com',
-  databaseURL: 'https://tracking-59c9e.firebaseio.com',
-  projectId: 'tracking-59c9e',
-  storageBucket: 'tracking-59c9e.appspot.com',
-  messagingSenderId: '306448410357'
+
 }
 firebase.initializeApp(config)
 
